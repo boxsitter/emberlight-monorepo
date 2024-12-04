a6f55539bccec987faa376087d5dccd65e5b301a
LeytonHouck <leyton.houck@gmail.com>
2024-12-04 01:06:25 -0800

    Basic navigation

--- packages/bess_ui/lib/app.dart
+++ packages/bess_ui/lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'utils/constants/colors.dart';
+//import 'utils/constants/colors.dart';
 import 'utils/constants/text_strings.dart';
 import 'utils/device/web_material_scroll.dart';
 import 'utils/theme/theme.dart';
@@ -18,7 +18,17 @@ class App extends StatelessWidget {
       darkTheme: BessieAppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
       scrollBehavior: MyCustomScrollBehavior(),
-      home: const FirstScreen(),
+      routes: {
+        '/': (context) => const FirstScreen(),
+        '/second-screen': (context) => const SecondScreen(),
+      },
+
+      getPages: [
+        GetPage(name: '/', page: () => const FirstScreen()),
+        GetPage(name: '/second-screen', page: () => const SecondScreen()),
+        GetPage(name: '/second-screen/:userId', page: () => const SecondScreen()),
+      ],
+      // home: const FirstScreen(),
     );
   }
 }
@@ -37,6 +47,7 @@ class FirstScreen extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+            /// SIMPLE NAVIGATION
             const Text(
               'Simple Navigation: Default Flutter Navigator VS GetX Navigation',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
@@ -63,6 +74,75 @@ class FirstScreen extends StatelessWidget {
                   child: const Text('GetX Navigation'),
               ),
             ),
+
+            /// NAMED NAVIGATION
+            const SizedBox(height: 50),
+            const Divider(),
+            const Text(
+              'Named Navigation: Flutter Navigator VS GetX Named Navigation',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/second-screen');
+                },
+                child: const Text('Default Named Navigation'),
+              ),
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {
+                  Get.toNamed('/second-screen');
+                },
+                child: const Text('GetX Named Navigation'),
+              ),
+            ),
+
+            /// PASS DATA
+            const SizedBox(height: 50),
+            const Divider(),
+            const Text(
+              'Pass Data between screens - GetX',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {
+                  Get.toNamed('/second-screen', arguments: 'GetX is fun');
+                },
+                child: const Text('GetX Pass Data'),
+              ),
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {
+                  Get.toNamed('/second-screen?device=phone&id=345&name=Leyton');
+                },
+                child: const Text('Pass Data in URL'),
+              ),
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              width: 280,
+              child: ElevatedButton(
+                onPressed: () {
+                  Get.toNamed(
+                    '/second-screen?device=phone&id=345&name=Leyton',
+                    arguments: 'GetX is fun!',
+                  );
+                },
+                child: const Text('Pass Data in URL with arguments'),
+              ),
+            ),
           ],
         ),
       ),
@@ -80,6 +160,19 @@ class SecondScreen extends StatelessWidget {
         title: const Text('Second Screen'),
         centerTitle: true,
       ),
+
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(Get.arguments ?? ''),
+
+            Text('Device = ${Get.parameters['device'] ?? ''}'),
+            Text('ID = ${Get.parameters['id'] ?? ''}'),
+            Text('Name = ${Get.parameters['name'] ?? ''}'),
+          ]
+        )
+      )
     );
   }
 }
