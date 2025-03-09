364ffd09e5c391b747b0634113b9b9a647e33e28
LeytonHouck <leyton.houck@gmail.com>
2025-03-08 18:01:18 -0800

    Removed firebase to start with a clean slate

--- packages/bess_ui/lib/common/routes/routes_middleware.dart
+++ packages/bess_ui/lib/common/routes/routes_middleware.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-import '../../data/repositories.authentication/authentication_repository.dart';
-import 'routes.dart';
-
-class BessRouteMiddleware extends GetMiddleware {
-  @override
-  RouteSettings? redirect(String? route) {
-    return AuthenticationRepository.instance.isAuthenticated
-        ? null
-        : const RouteSettings(name: BessRoutes.login);
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+//
+// import '../../data/repositories.authentication/authentication_repository.dart';
+// import 'routes.dart';
+//
+// class BessRouteMiddleware extends GetMiddleware {
+//   @override
+//   RouteSettings? redirect(String? route) {
+//     return AuthenticationRepository.instance.isAuthenticated
+//         ? null
+//         : const RouteSettings(name: BessRoutes.login);
+//   }
+// }
--- packages/bess_ui/lib/data/repositories.authentication/authentication_repository.dart
+++ packages/bess_ui/lib/data/repositories.authentication/authentication_repository.dart
@@ -1,97 +1,97 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/services.dart';
-import 'package:get/get.dart';
-
-import '../../common/routes/routes.dart';
-import '../../common/utils/exceptions/firebase_auth_exceptions.dart';
-import '../../common/utils/exceptions/firebase_exceptions.dart';
-import '../../common/utils/exceptions/format_exceptions.dart';
-import '../../common/utils/exceptions/platform_exceptions.dart';
-
-class AuthenticationRepository extends GetxController {
-  static AuthenticationRepository get instance => Get.find();
-
-  // Firebase Auth Instance
-  final _auth = FirebaseAuth.instance;
-
-  // Get Authenticated User Data
-  User? get authUser => _auth.currentUser;
-
-  // Get IsAuthenticated User
-  bool get isAuthenticated => _auth.currentUser != null;
-
-  // @override
-  // void onReady() {
-  //   _auth.setPersistence(Persistence.LOCAL);
-  // }
-
-  // Function to determine that relevant screen and redirect accordingly
-  void screenRedirect() async {
-    final user = _auth.currentUser;
-
-    // If the user is logged in
-    if (user != null) {
-      final user = _auth.currentUser;
-
-      // If the user is logged in
-      if (user != null) {
-        // Navigate to the home screen
-        Get.offAllNamed(BessRoutes.home);
-      } else {
-        Get.offAllNamed(BessRoutes.login);
-      }
-    }
-  }
-
-  // Login
-  Future<UserCredential> loginWithEmailAndPassword(
-      String email, String password) async {
-    try {
-      return await _auth.signInWithEmailAndPassword(
-          email: email, password: password);
-    } on FirebaseAuthException catch (e) {
-      throw BessFirebaseAuthException(e.code).message;
-    } on FirebaseException catch (e) {
-      throw BessFirebaseException(e.code).message;
-    } on FormatException catch (_) {
-      throw const BessFormatException();
-    } on PlatformException catch (e) {
-      throw BessPlatformException(e.code).message;
-    } catch (e) {
-      throw 'Something went wrong. Please try again';
-    }
-  }
-
-  // Register
-  Future<UserCredential> registerWithEmailAndPassword(
-      String email, String password) async {
-    try {
-      return await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
-    } on FirebaseAuthException catch (e) {
-      throw BessFirebaseAuthException(e.code).message;
-    } on FirebaseException catch (e) {
-      throw BessFirebaseException(e.code).message;
-    } on FormatException catch (_) {
-      throw const BessFormatException();
-    } on PlatformException catch (e) {
-      throw BessPlatformException(e.code).message;
-    } catch (e) {
-      throw 'Something went wrong. Please try again';
-    }
-  }
-
-// Register user by admin
-
-// Email verification
-
-// Forget Password
-
-// Forget Password
-
-// Re Authenticate User
-
-// Logout User
-
-// Delete User
-}
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/services.dart';
+// import 'package:get/get.dart';
+//
+// import '../../common/routes/routes.dart';
+// import '../../common/utils/exceptions/firebase_auth_exceptions.dart';
+// import '../../common/utils/exceptions/firebase_exceptions.dart';
+// import '../../common/utils/exceptions/format_exceptions.dart';
+// import '../../common/utils/exceptions/platform_exceptions.dart';
+//
+// class AuthenticationRepository extends GetxController {
+//   static AuthenticationRepository get instance => Get.find();
+//
+//   // Firebase Auth Instance
+//   final _auth = FirebaseAuth.instance;
+//
+//   // Get Authenticated User Data
+//   User? get authUser => _auth.currentUser;
+//
+//   // Get IsAuthenticated User
+//   bool get isAuthenticated => _auth.currentUser != null;
+//
+//   // @override
+//   // void onReady() {
+//   //   _auth.setPersistence(Persistence.LOCAL);
+//   // }
+//
+//   // Function to determine that relevant screen and redirect accordingly
+//   void screenRedirect() async {
+//     final user = _auth.currentUser;
+//
+//     // If the user is logged in
+//     if (user != null) {
+//       final user = _auth.currentUser;
+//
+//       // If the user is logged in
+//       if (user != null) {
+//         // Navigate to the home screen
+//         Get.offAllNamed(BessRoutes.home);
+//       } else {
+//         Get.offAllNamed(BessRoutes.login);
+//       }
+//     }
+//   }
+//
+//   // Login
+//   Future<UserCredential> loginWithEmailAndPassword(
+//       String email, String password) async {
+//     try {
+//       return await _auth.signInWithEmailAndPassword(
+//           email: email, password: password);
+//     } on FirebaseAuthException catch (e) {
+//       throw BessFirebaseAuthException(e.code).message;
+//     } on FirebaseException catch (e) {
+//       throw BessFirebaseException(e.code).message;
+//     } on FormatException catch (_) {
+//       throw const BessFormatException();
+//     } on PlatformException catch (e) {
+//       throw BessPlatformException(e.code).message;
+//     } catch (e) {
+//       throw 'Something went wrong. Please try again';
+//     }
+//   }
+//
+//   // Register
+//   Future<UserCredential> registerWithEmailAndPassword(
+//       String email, String password) async {
+//     try {
+//       return await _auth.createUserWithEmailAndPassword(
+//           email: email, password: password);
+//     } on FirebaseAuthException catch (e) {
+//       throw BessFirebaseAuthException(e.code).message;
+//     } on FirebaseException catch (e) {
+//       throw BessFirebaseException(e.code).message;
+//     } on FormatException catch (_) {
+//       throw const BessFormatException();
+//     } on PlatformException catch (e) {
+//       throw BessPlatformException(e.code).message;
+//     } catch (e) {
+//       throw 'Something went wrong. Please try again';
+//     }
+//   }
+//
+// // Register user by admin
+//
+// // Email verification
+//
+// // Forget Password
+//
+// // Forget Password
+//
+// // Re Authenticate User
+//
+// // Logout User
+//
+// // Delete User
+// }
