ea083e2cd150013ba02816c7a2289f7fa5ebadae
LeytonHouck <leyton.houck@gmail.com>
2025-04-01 00:54:16 -0700

    Initial commit

---
+++ packages/ember_fire/lib/ember_fire.dart
@@ -0,0 +1,12 @@
+/// Support for doing something awesome.
+///
+/// More dart docs go here.
+library;
+
+export 'src/repositories/authentication_repository.dart';
+export 'src/repositories/delete_repository.dart';
+export 'src/repositories/live_data_repository.dart';
+export 'src/repositories/pull_repository.dart';
+export 'src/repositories/push_repository.dart';
+
+// TODO: Export any libraries intended for clients of this package.
