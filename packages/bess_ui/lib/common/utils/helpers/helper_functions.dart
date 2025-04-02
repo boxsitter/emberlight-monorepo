cfa549a6c33dc50e9ba233154c96f60a9c684d54
LeytonHouck <leyton.houck@gmail.com>
2025-04-01 19:50:52 -0700

    work on integrating the 3 module system together

--- packages/bess_ui/lib/common/utils/helpers/helper_functions.dart
+++ packages/bess_ui/lib/common/utils/helpers/helper_functions.dart
@@ -137,18 +137,4 @@ class BessHelperFunctions {
       wrappedList.add(Row(children: rowChildren));
     }
     return wrappedList;
-  }
-
-  static DateTime parseDate(dynamic value) {
-    if (value is Timestamp) {
-      return value.toDate();
-    } else if (value is String) {
-      return DateTime.parse(value);
-    }
-    throw Exception("Invalid date type: ${value.runtimeType}");
-  }
-
-  static void updateDocumentTimestamp(Map<String, dynamic> json) {
-    json['updatedAt'] = Timestamp.fromDate(DateTime.now().toUtc());
-  }
 }
