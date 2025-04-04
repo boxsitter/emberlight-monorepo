9314f89da39863dfc1251ce8adde5522cd6eda31
LeytonHouck <leyton.houck@gmail.com>
2025-04-03 17:45:02 -0700

    Remove timestamp usage

--- packages/ember_core/lib/src/utils/helper_functions.dart
+++ packages/ember_core/lib/src/utils/helper_functions.dart
@@ -1,17 +1,30 @@
+class CoreHelperFunctions {
+  static DateTime parseDate(dynamic value) {
+    if (value == null) {
+      throw Exception('Invalid date type: null');
+    }
 
+    // Check the type name at runtime, since we can't import Timestamp
+    final typeName = value.runtimeType.toString();
+    if (typeName == 'Timestamp') {
+      // We assume it has a .toDate() method returning a DateTime
+      //  Use a dynamic call so we don't need the Timestamp type
+      final dt = (value as dynamic).toDate() as DateTime;
+      return dt.toUtc();
+    } else if (value is DateTime) {
+      // If we actually get a DateTime, just print and convert to UTC
+      print('parseDate received a direct DateTime: $value');
+      return value.toUtc();
+    } else if (value is String) {
+      // Possibly an ISO 8601 string
+      return DateTime.parse(value).toUtc();
+    }
 
-// class BessHelperFunctions {
-//   // TODO: fix this, don't use timestamp anymore
-//   static DateTime parseDate(dynamic value) {
-//     if (value is Timestamp) {
-//       return value.toDate();
-//     } else if (value is String) {
-//       return DateTime.parse(value);
-//     }
-//     throw Exception("Invalid date type: ${value.runtimeType}");
-//   }
-//
-//   static void updateDocumentTimestamp(Map<String, dynamic> json) {
-//     json['updatedAt'] = Timestamp.fromDate(DateTime.now().toUtc());
-//   }
-//}
+    throw Exception('Invalid date type: ${value.runtimeType}');
+  }
+
+  static void updateDocumentTimestamp(Map<String, dynamic> json) {
+    // For "updatedAt", we store an ISO 8601 string
+    json['updatedAt'] = DateTime.now().toUtc().toIso8601String();
+  }
+}
