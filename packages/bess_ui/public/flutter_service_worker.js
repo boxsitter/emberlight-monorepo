09fb4a5c357068355c4d3ff3552414ce929dfa17
LeytonHouck <leyton.houck@gmail.com>
2025-04-27 03:00:48 -0700

    new web build

--- packages/bess_ui/public/flutter_service_worker.js
+++ packages/bess_ui/public/flutter_service_worker.js
@@ -100,7 +100,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "59fe87b6dc3a768a52ea095c55dec149
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "05946e794ab73b2c2e4c1a040b656ddc",
+"flutter_bootstrap.js": "fd9cd58809b7e78547d983bcee90bf10",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
