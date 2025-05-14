32243442eb78badd6c7a1a8065a63aa718362367
LeytonHouck <leyton.houck@gmail.com>
2025-05-13 23:42:15 -0700

    Work on emdev

--- projects/emdev/lib/src/commands/release_command.dart
+++ projects/emdev/lib/src/commands/release_command.dart
@@ -19,37 +19,6 @@ class ReleaseCommand extends EmberCommand<void> {
 
   @override
   FutureOr<void> run() async {
-    // Access arguments using argResults
-    // final isDryRun = argResults?['dry-run'] as bool?;
-    // final packageName = argResults?['package'] as String?;
 
-    // For now, just print a message.
-    // Later, you can use CliInput and CliOutput from your ember_cli_utils
-    // final output = CliOutput();
-    // final input = CliInput();
-
-    print('Release command is called!');
-    print('Implementation for the release process will go here.');
-
-    // Example of how you might use argResults:
-    // if (packageName != null) {
-    //   output.info('Attempting to release package: $packageName');
-    // } else {
-    //   output.info('Processing release for all applicable packages.');
-    // }
-
-    // if (isDryRun ?? false) {
-    //   output.warning('Performing a dry run. No actual changes will be made.');
-    // }
-
-    // Add your release logic here.
-    // This could involve:
-    // - Reading package versions
-    // - Prompting for new version numbers
-    // - Updating pubspec.yaml files
-    // - Running tests
-    // - Building packages
-    // - Creating git tags
-    // - Publishing packages
   }
 }
---
+++ projects/emdev/lib/src/constants.dart
@@ -0,0 +1,10 @@
+class Constants {
+  // Relative paths
+  static const String repoRootPath = '../../';
+  static const String projectsDir = '${repoRootPath}projects';
+  static const String packagesDir = '${repoRootPath}packages';
+}
+
+class Text {
+  static const String projectSelection = 'Select a project to manage';
+}
